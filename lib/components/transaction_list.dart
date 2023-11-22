adf8d49113cd9a3df1f311f74d2ad18a1d2c39bc
Caio Andreta <caioandreta@gmail.com>
2023-11-21 23:54:02 -0300

    adicionados estilos de cor, texto e nova imagem de lista vazia

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -11,49 +11,67 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
-        itemCount: transactions.length,
-        itemBuilder: (ctx, index) {
-          final e = transactions[index];
-          return Card(
-            child: Row(children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 15,
-                  vertical: 10,
+      child: transactions.isEmpty
+          ? Column(
+              children: [
+                const SizedBox(height: 20),
+                Text(
+                  'Nenhuma Transação Cadastrada!',
+                  style: Theme.of(context).textTheme.titleLarge,
                 ),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Colors.purple,
-                  width: 2,
-                )),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  'R\$ ${e.value.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
+                const SizedBox(height: 50),
+                SizedBox(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
                   ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    e.title,
-                    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    DateFormat('d MMM y').format(e.date),
-                    style: TextStyle(color: Colors.grey.shade600),
-                  )
-                ],
-              )
-            ]),
-          );
-        },
-      ),
+                )
+              ],
+            )
+          : ListView.builder(
+              itemCount: transactions.length,
+              itemBuilder: (ctx, index) {
+                final e = transactions[index];
+                return Card(
+                  child: Row(children: [
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                        horizontal: 15,
+                        vertical: 10,
+                      ),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).colorScheme.primary,
+                        width: 2,
+                      )),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                        'R\$ ${e.value.toStringAsFixed(2)}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          e.title,
+                          style: Theme.of(context).textTheme.titleMedium,
+                        ),
+                        Text(
+                          DateFormat('d MMM y').format(e.date),
+                          style: Theme.of(context).textTheme.titleSmall,
+                        )
+                      ],
+                    )
+                  ]),
+                );
+              },
+            ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,44 @@ class DespesasApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
+    final ThemeData tema = ThemeData();
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.pinkAccent,
+          onPrimary: Colors.indigo.shade900,
+          secondary: Colors.amber,
+          onSecondary: Colors.indigo.shade900,
+        ),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          titleMedium: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          titleSmall: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+            color: Colors.grey.shade500,
+          ),
+        ),
+        appBarTheme: const AppBarTheme(
+          titleTextStyle: TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 20,
+            color: Colors.black,
+          ),
+        ),
+      ),
     );
   }
 }
@@ -26,10 +62,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final _transactions = [
-    Transaction(id: 't1', title: 'Novo Tênis de Corrida', value: 599.90, date: DateTime.now()),
-    Transaction(id: 't2', title: 'Bolo de Pote', value: 14.90, date: DateTime.now()),
-    Transaction(id: 't3', title: 'Calça', value: 249.99, date: DateTime.now()),
+  final List<Transaction> _transactions = [
+    // Transaction(id: 't1', title: 'Novo Tênis de Corrida', value: 599.90, date: DateTime.now()),
+    // Transaction(id: 't2', title: 'Bolo de Pote', value: 14.90, date: DateTime.now()),
+    // Transaction(id: 't3', title: 'Calça', value: 249.99, date: DateTime.now()),
   ];
 
   _addTransaction(String title, double value) {
@@ -63,7 +99,10 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [
           IconButton(
             onPressed: () => _openTransactionFormModal(context),
-            icon: const Icon(Icons.add),
+            icon: Icon(
+              Icons.add,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
           )
         ],
       ),
@@ -79,8 +118,10 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        child: const Icon(Icons.add),
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        foregroundColor: Theme.of(context).colorScheme.onSecondary,
         onPressed: () => _openTransactionFormModal(context),
+        child: const Icon(Icons.add),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
